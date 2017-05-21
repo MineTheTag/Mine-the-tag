0afd15b016a81ee2a16eafe57b0f057b80bd8bee
Omair95 <Omair95@protonmail.com>
2017-05-21 15:01:43 +0200

    Altres

--- app/src/main/java/com/example/omair/minethetag/CaptureTagActivity.java
+++ app/src/main/java/com/example/omair/minethetag/CaptureTagActivity.java
@@ -18,6 +18,7 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
@@ -179,6 +180,24 @@ public class CaptureTagActivity extends AppCompatActivity {
                 return headers;
             }
         };
+        MyStringRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 50000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
         MyRequestQueue.add(MyStringRequest);
     }
+
+
 }
